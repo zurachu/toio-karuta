5f2bc22fc331666fed7fc994ea13fcb32ec019de
zurachu <zurachu@gmail.com>
2021-01-07 22:39:31 +0900

    画面デザイン調整、解像度変更対応、UnityEditor実行では背面のCubeを操作できるようにUIを半透明にする

--- Assets/Scripts/Scenes/SampleScene.cs
+++ Assets/Scripts/Scenes/SampleScene.cs
@@ -1,14 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SampleScene : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private CanvasScaler canvasScaler;
     [SerializeField] private TitleView titleView;
     [SerializeField] private InGameView inGameView;
 
     private void Start()
     {
+#if UNITY_EDITOR
+        canvasGroup.alpha = 0.5f;
+#else
+        canvasGroup.alpha = 1f;
+#endif
+
         UIUtility.TrySetActive(titleView, true);
         UIUtility.TrySetActive(inGameView, false);
+    }
+
+    private void Update()
+    {
+        var screenAspectRatio = (float)Screen.width / Screen.height;
+        var canvasAspectRatio = canvasScaler.referenceResolution.x / canvasScaler.referenceResolution.y;
+        canvasScaler.matchWidthOrHeight = (screenAspectRatio < canvasAspectRatio) ? 0 : 1;
     }
 
     public void OnClickStart()
